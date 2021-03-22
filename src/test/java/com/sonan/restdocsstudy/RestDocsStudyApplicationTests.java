ce18ec25d29dfa906ea085976012d49558a89685
AnJaeSeongS2 <jindam91@naver.com>
2021-03-22 22:39:45 +0900

    first commit with boilerplate from spring initializer

---
+++ src/test/java/com/sonan/restdocsstudy/RestDocsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonan.restdocsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
